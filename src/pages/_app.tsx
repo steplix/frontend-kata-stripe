0d44542b653e5e4fc240664ce12e04ad876e7b30
Lautaro <lrozenblum@steplix.com>
2023-11-14 17:52:26 -0300

    [kata-stripe] removed footer image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />;
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type {NextPage} from "next";
 
 import Head from "next/head";
 
@@ -13,12 +13,7 @@ const Home: NextPage = () => {
       </Head>
       <header className="bg-[#180228] w-full">
         <nav className="w-full flex items-center justify-start p-4 border-white border-b-[0.5px]">
-          <img
-            alt="Steplix logo"
-            height="30px"
-            src="/assets/images/steplix.svg"
-            width="120px"
-          />
+          <img alt="Steplix logo" height="30px" src="/assets/images/steplix.svg" width="120px" />
         </nav>
       </header>
       <main className="grid lg:grid-cols-[300px,1fr] grid-rows-[100px,1fr] gap-4 lg:grid-rows-1 w-full bg-[#180228] mt-4">
@@ -31,17 +26,16 @@ const Home: NextPage = () => {
               Steplix Front End Base
             </h1>
             <p className="lg:max-w-4xl max-w-xs">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perferendis consequuntur, dolores, deleniti ipsam iure blanditiis
-              eum quam cum distinctio quidem quis amet ut voluptatem dolorum
-              nisi! Esse inventore molestiae doloremque neque, sequi sunt cum
-              temporibus ipsam aliquid totam distinctio deleniti? Esse quod
-              fugit velit vitae. Dolorem odit quibusdam ipsum libero!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis consequuntur,
+              dolores, deleniti ipsam iure blanditiis eum quam cum distinctio quidem quis amet ut
+              voluptatem dolorum nisi! Esse inventore molestiae doloremque neque, sequi sunt cum
+              temporibus ipsam aliquid totam distinctio deleniti? Esse quod fugit velit vitae.
+              Dolorem odit quibusdam ipsum libero!
             </p>
           </article>
         </section>
       </main>
-      <footer className="w-full flex items-center justify-center p-4 bg-[#180228] border-white border-t-[0.5px] mt-4"></footer>
+      <footer className="w-full flex items-center justify-center p-4 bg-[#180228] border-white border-t-[0.5px] mt-4" />
     </div>
   );
 };
