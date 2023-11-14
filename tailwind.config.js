785f18e9de60fda0fbc78b6e2603d34cdfd6730b
Lautaro <lrozenblum@steplix.com>
2023-11-14 17:51:48 -0300

    [kata-stripe] cleaning project and updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{ts,tsx,js,jsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
+  media: false,
   theme: {
     extend: {},
   },
